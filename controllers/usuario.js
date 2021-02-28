4dd9f3d4d33c561aa5c2cb32729bb22a99dcd525
Hanni <hannia.tlatenchi@gmail.com>
2021-02-27 18:43:07 -0600

    usuario_controller

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,1 +1,63 @@
 const Usuario = require('../models/usuario')
+const Libros = require('../models/libros')
+
+function registroUsuario(req,res){
+    var usuario =new Usuario(req.body)
+    res.status(201).send(usuario)
+}
+
+function editarUsuario(req,res){
+    var usuario1 = new Usuario(req.params.id, 'Carlos', 'Pardo', 'direccion1','foto1','contraseña1')
+    var modificaciones = req.body
+    usuario1 = {...usuario1, ...modificaciones}
+    res.send(usuario1)
+    
+}
+
+function registroLibros(req, res){
+    var libro = new Libros(req.body)
+    res.status(201).send(libro)
+}
+
+function intercambiarLibros(req, res){   
+    var libro1 =new Libros(1, 'Harry Potter', 'J.K. Rowling')
+    var libro2 =new Libros(2, 'El señor de los anillos', 'J.R.R. Tolkien')
+
+}
+
+function evaluarPropuestaDeIntercambio(req, res){
+    
+    if(TRUE){
+        res.send('Intercambio aceptado')
+    }else{
+        res.send('Intercambio no aceptado')
+    }
+    
+}
+
+function evaluarUsuarios(req, res){
+    var usuario = new Usuario(req.params.id, 'Usuario1', 'apellido')
+    usuario.calificacion = req.body.calificacion;
+    usuario.comentario = req.body.comentario;
+    res.status(200).send('Usuario evaluado')
+}
+
+function evaluarLibro(req,res){
+    var libro = new Libros(req.params.id, 'libro1', 'autor1')
+    libro.calificacion = req.body.calificacion;
+    libro.comentario = req.body.comentario;
+    res.status(200).send('Libro evaluado')
+
+}
+
+function comprar(req, res){
+    var libro = new Libros(req.params.id, 'libro1')
+    libro.stock--
+    res.status(200).send('libro comprado')
+}
+
+function visualizarlibros(){
+    var libro1 = new Libros(1, 'Harry Potter', 'J.K. Rowling')
+    var libro2 = new Libros(2, 'El señor de los anillos', 'J.R.R. Tolkien')
+    res.send([libro1, libro2])
+}
--- models/usuario.js
+++ models/usuario.js
@@ -1,7 +1,7 @@
 /** Clase que representa a un usurario */
 
 class Usuario{
-    constructor(id, nombre, apellido, direccion, fotoDeUsuario, contraseña, email, calificacionUsuarioComentario, librosEnIntercambio, historialDeIntercambios, telefono){
+    constructor(id, nombre, apellido, direccion, fotoDeUsuario, contraseña, email, calificacion, comentario, librosEnIntercambio, historialDeIntercambios, telefono){
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -9,52 +9,12 @@ class Usuario{
         this.fotoDeUsuario = fotoDeUsuario;
         this.contraseña = contraseña;
         this.email = email;
-        this.calificacionUsuarioComentario = calificacionUsuarioComentario; // Calificación que recibe un usuario de otros usuarios
+        this.calificacion = calificacion;
+        this.comentario = comentario; // Calificación que recibe un usuario de otros usuarios
         this.librosEnIntercambio = librosEnIntercambio; // Libros que posee para intercambio
         this.historialDeIntercambios = historialDeIntercambios; // Historial de intercambios realizados
         this.telefono = telefono;
     }
-
-    registroUsuario(){
-        // Función para el registro de usuario en la plataforma
-    }
-
-    editarUsuario(){
-        
-    }
-
-    registroLibros(){
-        // función para el registro de libros a intercambiar
-    }
-
-    intercambiarLibros(){
-        // función para generar comunicación entre usuarios - propuesta de intercambio   
-    }
-
-    evaluarPropuestaDeIntercambio(){
-        // función para que el usuario pueda decidir en aceptar, rechazar o cancelar un intercambio   
-    }
-
-    evaluarUsuarios(){
-        // función para que el usuario pueda evaluar el trato o credibilidad de propuesta de otros usuarios
-    }
-
-    evaluarLibro(){
-        // función para que el usuaurio califique libros
-    }
-
-    comprar(){
-        // función para comprar un libro
-    }
-
-    evaluarCompra(){
-        // función para aceptar o declinar compra
-    }
-
-    visualizarlibros(){
-        // función para ver el catalogo de libros
-    }
-
     
 
 }
