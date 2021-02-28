1c24cbfe0b8e7474fb3ada15cf21c07a17480943
sebastianneri <62320232+sebastianneri@users.noreply.github.com>
2021-02-27 19:53:59 -0600

    Intercambio

---
+++ controllers/intercambio.js
@@ -0,0 +1,31 @@
+const Intercambio = require("../models/intercambio")
+const Usuario = require('../models/usuario')
+const Libros = require('../models/libros')
+
+function intercambiarLibros(req, res){
+    var usuario1 = new Usuario(req.params.id, 'Carlos', 'Pardo', 'direccion1','foto1','contraseña1')
+    var usuario2 = new Usuario(req.params.id, 'Sebastian', 'Neri', 'direccion2','foto2','contraseña2')
+    var libro1 = new Libros(1, 'Harry Potter', 'J.K. Rowling')
+    var libro2 = new Libros(2, 'El señor de los anillos', 'J.R.R. Tolkien')
+    var intercambio = new Intercambio(usuario1, usuario2, libro1, libro2, true, false, "La Roma")
+    if (intercambio.status_general){
+        res.status(200).send(`Intercambio Realizado\nEl punto de encuentro es: ${intercambio.punto_medio}`)
+    }else{
+        res.status(200).send("Lamentablemente el intercambio aún no se ha realizado.")
+    }
+}
+
+function cancelarIntercambio(req, res){
+    var usuario1 = new Usuario(req.params.id, 'Carlos', 'Pardo', 'direccion1','foto1','contraseña1')
+    var usuario2 = new Usuario(req.params.id, 'Sebastian', 'Neri', 'direccion2','foto2','contraseña2')
+    var libro1 = new Libros(1, 'Harry Potter', 'J.K. Rowling')
+    var libro2 = new Libros(2, 'El señor de los anillos', 'J.R.R. Tolkien')
+    var intercambio = new Intercambio(usuario1, usuario2, libro1, libro2, true, true, "La Roma")
+    intercambio.status_general = false
+    res.status(200).send("Se ha cancelado el intercambio exitosamente")
+}
+
+module.export= {
+    intercambiarLibros,
+    cancelarIntercambio
+}
---
+++ models/intercambio.js
@@ -0,0 +1,14 @@
+class Intercambio{
+    constructor(usuario1, usuario2, libro1, libro2, status1, status2, punto_medio){
+        this.usuario1 = usuario1;
+        this.usuario2 = usuario2;
+        this.libro1 = libro1;
+        this.libro2 = libro2;
+        this.status1 = status1;
+        this.status2 = status2;
+        this.status_general = this.status1 && this.status2;
+        this.punto_medio = punto_medio;
+    }
+}
+
+module.export = Intercambio
